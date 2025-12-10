c7ef7e6b54544f84ce3803fe29b1714e5fe76478
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-12-10 21:28:40 +0900

    study: program's structure

--- CppProject/Chapter01_01/main.cpp
+++ CppProject/Chapter01_01/main.cpp
@@ -1,7 +1,14 @@
-﻿#include <bits/stdc++.h>
+﻿#include <iostream>
 
 using namespace std;
 
-int main() {
-    cout << "hello" << endl;
+int main()
+{
+    int x = 2;
+    x = 5;
+    int y = x + 3;
+
+    std::cout << x + y << std::endl;
+
+    return 0;
 }
