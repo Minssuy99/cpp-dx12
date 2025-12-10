fa9a14c2143fbe917e0b940fb6cd6ec56fd3ddc1
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-12-11 01:52:47 +0900

    study: comments

---
+++ CppProject/Chapter01_02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * clion shortcut : ctrl + /, ctrl + shift + /
+ * VS2022 shortcut : ctrl + k + c, ctrl + k + u
+ */
+
+int main()
+{
+    cout << "this is comment" << endl;
+
+    return 0;
+}
