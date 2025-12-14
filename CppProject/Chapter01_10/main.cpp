2cbebd0390d777d7cf3db42cd5841f7a7794c98c
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-12-14 20:29:35 +0900

    study: declaration and definition

---
+++ CppProject/Chapter01_10/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+// 전방 선언 forward declaration
+int add(int a, int b);
+int multiply(int a, int b);
+int subtract(int a, int b);
+
+int main()
+{
+    cout << add(1, 2) << endl;
+
+    return 0;
+}
+
+// 정의 definition
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
